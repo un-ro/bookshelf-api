7526872ca4e270827d53f70d27b27eb4d04e694a
Unero <ramadhanbhg@gmail.com>
2021-05-29 00:41:37 +0700

    :white_check_mark: [Optional] Done

--- src/handler/getBooks.js
+++ src/handler/getBooks.js
@@ -1,14 +1,33 @@
 const books = require('./books');
 
-const getBooks = () => ({
-  status: 'success',
-  data: {
-    books: books.map((book) => ({
-      id: book.id,
-      name: book.name,
-      publisher: book.publisher,
-    })),
-  },
-  code: 200,
-});
+const getBooks = (request, h) => {
+  // [Optional] 1: Query
+  const { name, reading, finished } = request.query;
+  let queryBook = books;
+
+  if (name !== undefined) {
+    queryBook = queryBook.filter((item) => item.name.toLowercase().includes(name.toLowercase()));
+  }
+
+  if (reading !== undefined) {
+    queryBook = queryBook.filter((item) => item.reading === !!Number(reading));
+  }
+
+  if (finished !== undefined) {
+    queryBook = queryBook.filter((item) => item.finished === !!Number(finished));
+  }
+
+  const response = h.response({
+    status: 'success',
+    data: {
+      books: queryBook.map((book) => ({
+        id: book.id,
+        name: book.name,
+        publisher: book.publisher,
+      })),
+    },
+  });
+  response.code(200);
+  return response;
+};
 module.exports = getBooks;
